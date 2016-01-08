d33f82a62d0836fe8b3ac0f861f01e8a0cf0e9bf
unknown-user <unknown-user@fe80::45bb:a1c6:eb4c:8813%8>
2016-01-08 07:24:52 +0300

    Scythae. Fixing commit issues.

---
+++ Scripts/Bonuses/Buff.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum ShipModifierType {smtMoveSpeed, smtIncomingDamage, smtOutcomingDamage};
+
+public class Buff {
+	public class Modifier {
+		public ShipModifierType modifierType;
+		public float value;
+	}
+
+	public string caption;
+	public Modifier[] modifiers;
+	public float duration;
+	public float startTime;
+}
