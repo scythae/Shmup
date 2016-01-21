cf432e584c24ca92668637e3b74a73769fb8aa63
unknown-user <unknown-user@fe80::c8f7:5c7d:b789:2e30%8>
2016-01-21 04:07:07 +0300

    Scythae. Added scrolling background. Added scanning sprite for a pixels of special color.

---
+++ Scripts/SpriteScanner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScanner {
+	public static float error = 0;
+	public static bool needSameAlpha = true;
+
+	public SpriteScanner () {
+	}
+
+	public static List<Vector2> GetColoredPixelsCoords(Color color, GameObject gameObject) {
+		SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		if (spriteRenderer == null)
+			return null;
+		else
+			return GetColoredPixelsCoords(color, spriteRenderer.sprite);
+	}
+
+	public static List<Vector2> GetColoredPixelsCoords(Color color, Sprite sprite) {
+		if (sprite == null)
+			return null;
+		else
+			return GetColoredPixelsCoords(color, sprite.texture);
+	}
+
+	public static List<Vector2> GetColoredPixelsCoords(Color color, Texture2D texture2d) {
+		Color pixel;
+		List<Vector2> result = new List<Vector2> ();
+
+		Debug.Log(texture2d.width + "; " + texture2d.height);
+
+		for (int x = 0; x < texture2d.width; x++)
+			for (int y = 0; y < texture2d.height; y++) {
+				pixel = texture2d.GetPixel(x, y);
+
+				if (ColorsEqual(pixel, color)) {
+					result.Add(new Vector2(x, y));
+				}
+			}
+
+
+		return result;
+	}
+
+	private static bool ColorsEqual(Color color1, Color color2) {		
+		return (			
+			ApproximatelyEqual(color1.r, color2.r) &&
+			ApproximatelyEqual(color1.g, color2.g) &&
+			ApproximatelyEqual(color1.b, color2.b) &&
+			(ApproximatelyEqual(color1.a, color2.a) || !needSameAlpha)
+		);
+	}
+
+	private static bool ApproximatelyEqual(float x1, float x2) {
+		float diff = Mathf.Abs(x1 - x2);
+		return (Mathf.Approximately(diff, error) || (diff < error));
+	}
+}
+
---
+++ Scripts/UI/Scroller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Scroller: MonoBehaviour{
+	private GameObject copyTop;
+	private GameObject copyBottom;
+	private float itemHeight = 0;
+	public float scrollStepPerSecond = -0.05f;
+
+	private void Start () {
+		copyTop = Instantiate (this.gameObject);
+		copyTop.name = this.gameObject.name + "_Top";
+		Destroy (copyTop.GetComponent<Scroller> ());
+		copyTop.transform.SetParent(this.gameObject.transform.parent);
+
+		copyBottom = Instantiate (this.gameObject);
+		copyBottom.name = this.gameObject.name + "_Bottom";
+		Destroy (copyBottom.GetComponent<Scroller> ());
+		copyBottom.transform.SetParent(this.gameObject.transform.parent);
+
+		itemHeight = (this.gameObject.transform as RectTransform).rect.height;
+
+	}
+
+	private void Update () {
+		if (itemHeight <= 0)
+			return;
+
+		Vector3 newPosition = this.gameObject.transform.position;
+		newPosition.y = Mathf.Repeat(Time.time * scrollStepPerSecond, itemHeight);
+		this.gameObject.transform.position = newPosition;
+		copyTop.transform.position = newPosition + new Vector3 (0, itemHeight, 0);
+		copyBottom.transform.position = newPosition - new Vector3 (0, itemHeight, 0);
+	}
+}
